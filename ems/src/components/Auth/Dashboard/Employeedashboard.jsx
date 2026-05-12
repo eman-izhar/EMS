bc090b251b668e48456b65c31864a899204705d9
Eman Izhar <166126055+Eizhar2005@users.noreply.github.com>
2026-05-12 12:21:40 +0500

    1. form submit done

--- ems/src/components/Auth/Dashboard/Employeedashboard.jsx
+++ ems/src/components/Auth/Dashboard/Employeedashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Others/header'
+import TaskListNumbers from '../Others/TaskListNumbers'
 
 const Employeedashboard = () => {
   return (
     <div className='p-10 bg-[#1c1c1c] h-screen'>
       <Header/>
+      <TaskListNumbers/>    
     </div>
   )
 }
---
+++ ems/src/components/Auth/Others/TaskListNumbers.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskListNumbers = () => {
+  return (
+    <div className='flex h-screen justify-between gap-5 '>
+      <div className='H-40 w-1/2 bg-amber-300'>
+
+      </div>
+    </div>
+  )
+}
+
+export default TaskListNumbers
